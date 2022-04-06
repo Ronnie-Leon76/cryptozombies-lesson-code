958faaee4fc67649275140b61c87cd07b282f94a
Ronnie-Leon76 <ronleon76@gmail.com>
2022-04-06 11:31:35 +0300

    added contract info detail

--- lesson-6/chapter-9/erc721.sol
+++ lesson-6/chapter-9/erc721.sol
@@ -9,3 +9,4 @@ contract ERC721 {
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
   function approve(address _approved, uint256 _tokenId) external payable;
 }
+// Application Binary Interface 
